87a4aa289d85e720db69610f8a872f009a973b57
JacobJB08 <116194207+JacobJB08@users.noreply.github.com>
2023-08-25 12:30:13 -0700

    :fire:

--- QuickTaskP6/Assets/Scripts/PlayerController.cs
+++ QuickTaskP6/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
     Rigidbody2D rigidbody2d;
-    float horizontal;
-    float vertical;
     public float upForce = 200f;
+    public bool isOnGround = false;
+    public bool doubleJumpUsed = false;
+    public bool win = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,23 +20,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-        Vector2 move = new Vector2(horizontal, vertical);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            rigidbody2d.AddForce(new Vector2(0, upForce));
+            isOnGround = false;
+            doubleJumpUsed = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            rigidbody2d.AddForce(new Vector2(0, upForce));
+        }
     }
 
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-
-        rigidbody2d.MovePosition(position);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.D))
         {
-            rigidbody2d.velocity = Vector2.zero;
-            rigidbody2d.AddForce(new Vector2(0, upForce));
+            rigidbody2d.AddForce(new Vector2(speed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2d.AddForce(new Vector2(-speed, 0));
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isOnGround = true;
         }
     }
 }
+ 
